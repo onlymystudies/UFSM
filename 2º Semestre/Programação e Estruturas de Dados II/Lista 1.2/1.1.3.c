7294065fc4d77e4b7c48542f8c88691af7a3d821
Storage Leonardo Zamboni <73598408+storageleozamboni@users.noreply.github.com>
2020-10-29 10:41:32 -0300

    Create 1.1.3.c

---
+++ 2º Semestre/Programação e Estruturas de Dados II/Lista 1.2/1.1.3.c
@@ -0,0 +1,32 @@
+/* Problema 1.2.3
+ * Fazer um algoritmo que:
+ * Leia um número indeterminado de linhas contendo cada uma um número inteiro.
+ * A última linha, que não entrará nos cálculos, contém o valor do número igual a zero.
+ * Calcule e escreva, para cada número válido, se este é ou não um quadrado perfeito.
+ * Um número será quadrado perfeito quando respeitar a regra de formação: n2= a.
+ * Nessa regra, n é qualquer número inteiro positivo e a é o número quadrado perfeito. Ex.:
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int recursao_perfeito() {
+    float numero;
+    double quadrado;
+
+    printf("Numero:\n");
+    scanf("%f", &numero);
+    if (numero == 0) return 0;
+
+    quadrado = pow(numero, 2);
+    if (fmod(quadrado, 2) == 1 || fmod(quadrado, 2) == 0)
+        printf("%.1f e um quadrado perfeito\n", numero);
+    else
+        printf("%.1f nao e um quadrado perfeito\n", numero);
+    return recursao_perfeito();
+}
+
+int main() {
+    recursao_perfeito();
+    system("pause");
+}
