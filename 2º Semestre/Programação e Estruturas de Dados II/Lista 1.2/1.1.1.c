1afc90cbade46d7b5af03accea968c7c9b350da8
Storage Leonardo Zamboni <73598408+storageleozamboni@users.noreply.github.com>
2020-10-29 09:00:01 -0300

    Create 1.1.1.c

---
+++ 2º Semestre/Programação e Estruturas de Dados II/Lista 1.2/1.1.1.c
@@ -0,0 +1,33 @@
+/* Problema 1.2.1
+ * Fazer um algoritmo que:
+ * Leia um número indeterminado de linhas contendo cada uma um número inteiro.
+ * A última linha, que não entrará nos cálculos, contém o valor do número igual a zero.
+ * Calcule e escreva, para cada número válido, se este é ou não primo.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int recursao_primo();
+
+int main() {
+    recursao_primo();
+    system("pause");
+}
+
+int recursao_primo() {
+    int numero, resultado = 0;
+
+    printf("Numero:\n");
+    scanf("%d", &numero);
+    for (int i = 2; i <= numero / 2; ++i) {
+        if (numero % i == 0) {
+            resultado++;
+            break;
+        }
+    }
+
+    if (resultado > 0) printf("%d nao e um numero primo\n", numero);
+    else printf("%d e um numero primo\n", numero);
+    if (numero == 0) return 0;
+    else return recursao_primo();
+}
