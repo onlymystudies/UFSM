1d5e283c75b7532b4a292ef0d955fa8d66d41698
Leonardo Zamboni <67336436+leozamboni@users.noreply.github.com>
2020-11-24 15:47:26 -0300

    Create 1.6.c

---
+++ 2º Semestre/Programação e Estruturas de Dados II/Lista 6.1/1.6.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAM 10
+
+typedef struct cidades{
+    int anterior;
+    char cidade[16];
+    int proximo;
+}Cidades;
+
+typedef struct rodovias{
+    char rodovia[6];
+    Cidades cidades[TAM];
+    int proximo;
+}Rodovias;
+
+typedef struct lista{
+    int inicio;
+    Rodovias rodovias[TAM];
+    int final;
+}Lista;
+
+void criaCidades(Lista *lista) {
+    lista->rodovias[lista->final].cidades[lista->rodovias[lista->final].proximo].proximo = 0;
+    lista->rodovias[lista->final].cidades[lista->rodovias[lista->final].proximo].anterior = 0;
+}
+
+void criaRodovias(Lista *lista) {
+    lista->rodovias[lista->final].proximo = 0;
+    criaCidades(lista);
+}
+
+Lista *cria(void) {
+    Lista *lista = (Lista *) malloc(sizeof(Lista));
+    lista->inicio = lista->final = 0;
+    criaRodovias(lista);
+    return lista;
+}
+
+void insereRodovias(Lista *lista, char *rodovias) {
+    lista->final++;
+    strcpy(lista->rodovias[lista->final].rodovia, rodovias);
+    criaRodovias(lista);
+}
+
+void insereCidades(Lista *lista, char *cidade) {
+    lista->rodovias[lista->final].proximo++;
+    lista->rodovias[lista->final].cidades[lista->rodovias->proximo].proximo++;
+    strcpy(lista->rodovias[lista->final].cidades[lista->rodovias[lista->final].proximo].cidade, cidade);
+}
+
+void imprime(Lista *lista) {
+    for (size_t i = 1; i <= lista->final; ++i) {
+        printf("Rodovia %s\n", lista->rodovias[i].rodovia);
+        for (size_t j = 1; j <= lista->rodovias[i].proximo; ++j) {
+            printf("Cidades %s\n", lista->rodovias[i].cidades[j].cidade);
+        }
+        printf("\n");
+    }
+}
+
+void insereCidade(Lista *lista, char *rodovia, char *cidade) {
+    for (size_t i = 1; i <= lista->final; ++i) {
+        if (strcmp(lista->rodovias[i].rodovia, rodovia) == 0) {
+            lista->rodovias[i].proximo++;
+            for (size_t y = 1; y <= lista->rodovias[i].proximo; ++y) {
+                if (strcmp(lista->rodovias[i].cidades[y].cidade, cidade) >= 0) {
+                    for (size_t j = lista->rodovias[i].proximo; j > y; --j) {
+                        strcpy(lista->rodovias[i].cidades[j].cidade,
+                               lista->rodovias[i].cidades[j - 1].cidade);
+                    }
+                    strcpy(lista->rodovias[i].cidades[y].cidade, cidade);
+                    return;
+                }
+            }
+            strcpy(lista->rodovias[i].cidades[lista->rodovias[i].proximo].cidade, cidade);
+        }
+    }
+}
+
+Lista *rodoviasCidade(Lista *lista, char *cidade) {
+    Lista *listaRodovias = cria();
+    for (size_t i = 1; i <= lista->final; ++i)
+        for (size_t j = 1; j <= lista->rodovias[i].proximo; ++j) {
+            if (strcmp(lista->rodovias[i].cidades[j].cidade, cidade) == 0) {
+                insereRodovias(listaRodovias, lista->rodovias[i].rodovia);
+            }
+        }
+    return listaRodovias;
+}
+
+int main(void) {
+    Lista *lista = cria();
+    insereRodovias(lista, "BR123");
+    insereCidades(lista, "B");
+    insereCidades(lista, "C");
+    insereCidades(lista, "E");
+
+    insereRodovias(lista, "BR456");
+    insereCidades(lista, "D");
+    insereCidades(lista, "E");
+    insereCidades(lista, "F");
+
+    insereCidade(lista, "BR123", "D"); // B
+    insereCidade(lista, "BR123", "F");
+    insereCidade(lista, "BR123", "A");
+
+    Lista *listaRodovias = rodoviasCidade(lista, "aaaoo"); // C
+
+    imprime(lista);
+    system("pause");
+}
