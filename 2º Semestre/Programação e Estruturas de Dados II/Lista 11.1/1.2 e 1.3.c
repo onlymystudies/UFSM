1a671a6236a37111d5af06566f2167cb0a8c875e
Leonardo Zamboni <67336436+leozamboni@users.noreply.github.com>
2021-01-12 14:49:09 -0300

    Create 1.2 e 1.3.c

---
+++ 2º Semestre/Programação e Estruturas de Dados II/Lista 11.1/1.2 e 1.3.c
@@ -0,0 +1,202 @@
+/* Problema 2
+Implementar o algoritmo da pág. 161: “proc profr (v:nó)_”, versão recursiva:
+proc profr (v:nó)_
+var w:nó;
+inicio
+execute visite(v);
+execute marque(v);
+para cada w adjacente a v faça
+se !marcado(w) entao execute profr(w)
+fim
+Problema 3
+Implementar, em C, os seguintes procedimentos, das páginas 160 e 161:
+proc ampl (v:nó)_
+proc prof (V:nó)_ */
+#include <stdio.h>
+#include <stdlib.h>
+// O vetor de listas de adjac�ncia de um grafo tem uma lista encadeada (= linked list) associada com cada
+// v�rtice do grafo.  A lista associada com um v�rtice v cont�m todos os vizinhos de v.  Portanto, a lista
+// do v�rtice v representa o leque de sa�da de v.   Por exemplo, eis o vetor de listas de adjac�ncia do grafo
+//cujos arcos s�o  0-1 0-5 1-0 1-5 2-4 3-1 5-3 :
+
+//0: 5 1
+//1: 5 0
+//2: 4
+//3: 1
+//4:
+//5: 3
+
+/* V�rtices de grafos s�o representados por objetos do tipo vertex. */
+#define vertex int
+
+// Na representa��o por listas de adjac�ncia, um grafo � representado por uma struct graph que cont�m o vetor
+// de listas de adjac�ncia, o n�mero de v�rtices, e o n�mero de arcos do grafo:
+/* REPRESENTA��O POR LISTAS DE ADJAC�NCIA: A estrutura graph representa um grafo. O campo adj � um ponteiro
+para o vetor de listas de adjac�ncia, o campo V cont�m o n�mero de v�rtices e o campo A cont�m o n�mero de
+arcos do grafo. */
+typedef struct node *link;
+
+struct graph {
+    int V;
+    int A;
+    link *adj;
+};
+
+/* Um Graph � um ponteiro para um graph. */
+typedef struct graph *Graph;
+
+/* A lista de adjac�ncia de um v�rtice v � composta por n�s do tipo node. Cada n� da lista corresponde a um
+arco e cont�m um vizinho w de v e o endere�o do n� seguinte da lista. Um link � um ponteiro para um node. */
+struct node {
+    int marcado;
+    vertex w;
+    link next;
+};
+
+/* A fun��o NEWnode() recebe um v�rtice w e o endere�o next de um n� e devolve o endere�o a de um novo n�
+tal que a->w == w e a->next == next. */
+static link NEWnode( vertex w, link next) {
+    link a = malloc( sizeof (struct node));
+    a->w = w;
+    a->next = next;
+    return a;
+}
+
+/* REPRESENTA��O POR LISTAS DE ADJAC�NCIA: A fun��o GRAPHinit() constr�i um grafo com v�rtices 0 1 .. V-1
+e nenhum arco. */
+Graph GRAPHinit( int V) {
+    vertex v;
+    Graph G = malloc( sizeof *G);
+    G->V = V;
+    G->A = 0;
+    G->adj = malloc( V * sizeof (link));
+    for (v = 0; v < V; ++v)
+        G->adj[v] = NULL;
+    return G;
+}
+/* REPRESENTAcaO POR LISTAS DE ADJACeNCIA: A funcao GRAPHinsertArc() insere um arco v-w no grafo G.
+A funcao sup�e que v e w sao distintos, positivos e menores que G->V. Se o grafo ja tem um arco v-w,
+a funcao nao faz nada. */
+void GRAPHinsertArc( Graph G, vertex v, vertex w) {
+    link a;
+    for (a = G->adj[v]; a != NULL; a = a->next)
+        if (a->w == w) return;
+    G->adj[v] = NEWnode( w, G->adj[v]);
+    G->A++;
+}
+
+void GRAPHshow( Graph G) {
+    size_t i = 0;
+    while (i != G->V) {
+        printf("%d: ", i);
+        for (link a = G->adj[i]; a; a = a->next) {
+            printf("%d ", a->w);
+        }
+        puts("");
+        i++;
+    }
+}
+
+void GRAPHshowAll( Graph G) {
+    printf("   ");
+    for (size_t i = 0; i != G->V; ++i)
+        printf("%d ", i);
+    puts("");
+
+    for (size_t j = 0; j < G->V; ++j) {
+        printf("%d: ", j);
+        link a = G->adj[j];
+        for (size_t i = 0; i < G->V; ++i) {
+            if (a && a->w == i) {
+                printf("1 ");
+                a = a->next;
+            }
+            else
+                printf("0 ");
+        }
+        puts("");
+    }
+}
+
+void GRAPHremoveArc( Graph G, vertex v, vertex w) {
+    link a;
+    link anterior = NULL;
+    for (a = G->adj[v]; a != NULL; a = a->next) {
+        if (a->w == w)
+            break;
+        anterior = a;
+    }
+    if (anterior)
+        anterior->next = a->next;
+    else
+        G->adj[v] = a->next;
+    G->A--;
+}
+
+void buscaAmplitude(Graph G, vertex v) { // proc ampl (v:nó)_
+    if (v < G->V) {
+        for (size_t i = 0; i < G->V; ++i) {
+            for (link a = G->adj[i]; a != NULL; a = a->next) {
+                if (a->marcado) {
+                    printf( "%2d", a->w);
+                    a->marcado = 1;
+                }
+            }
+            puts("");
+        }
+    }
+}
+
+void buscaProfundidade(Graph G, vertex v) { // proc prof (V:nó)_
+    if (v < G->V) {
+        for (size_t i = 0; i < G->V; ++i) {
+            for (link a = G->adj[i]; a != NULL; a = a->next) {
+                printf( "%2d", a->w);
+            }
+            puts("");
+        }
+    }
+}
+
+void buscaProfundidadeRecursiva(Graph G, vertex v) { // proc profr (v:nó)_
+    link a;
+    if (v < G->V) {
+        for (a = G->adj[v]; a != NULL; a = a->next) {
+            printf( "%2d", a->w);	// visita
+            a->marcado = 1;	// 0 nao e 1 marcado
+        }
+        puts("");
+        v++;
+        buscaProfundidadeRecursiva(G, v); // executa profr...
+    }
+}
+
+int main() {
+//0: 5 1
+//1: 5 0
+//2: 4
+//3: 1
+//4:
+//5: 3
+    Graph G = GRAPHinit(6);
+    GRAPHinsertArc(G, 0, 5);
+    GRAPHinsertArc(G, 0, 1);
+    GRAPHinsertArc(G, 1, 5);
+    GRAPHinsertArc(G, 1, 0);
+    GRAPHinsertArc(G, 2, 4);
+    GRAPHinsertArc(G, 3, 1);
+    GRAPHinsertArc(G, 5, 3);
+    GRAPHshow(G);       puts("");
+    GRAPHshowAll(G);    puts("");
+    GRAPHremoveArc(G, 1, 5);
+    GRAPHshow(G);       puts("");
+    GRAPHshowAll(G);    puts("");
+    buscaProfundidadeRecursiva(G, 0);   puts("");
+    buscaProfundidade(G, 0);            puts("");
+    buscaAmplitude(G, 0);               puts("");
+    system("pause");
+    return 0;
+    //void GRAPHremoveArc( Graph G, vertex v, vertex w); // implementar
+    //void GRAPHshow( Graph G); // implementar
+    //void GRAPHshowAll( Graph G); // implementar
+}
